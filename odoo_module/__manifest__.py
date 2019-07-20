f903efe8a135ca1029d136af34711604bb4262ce
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2019-07-20 18:38:37 +0200

    Update __manifest__.py

--- odoo_module/__manifest__.py
+++ odoo_module/__manifest__.py
@@ -2,7 +2,7 @@
     "name": "odoo module",
     "version": "12.0.2.2.1",
     "author": "Author, Odoo Community Association (OCA)",
-    "summary": "Just extending the summary....",
+    "summary": "Just extending the summary.....",
     "license": "LGPL-3",
     "installable": True,
     "auto_install": False,
