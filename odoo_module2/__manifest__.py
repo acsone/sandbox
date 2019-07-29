8c74de87ec7297a2432f31743d4eb7236ce23dd0
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2019-07-29 17:13:07 +0200

    add module2

---
+++ odoo_module2/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "odoo module 2",
+    "version": "12.0.1.0.1",
+    "author": "Author, Odoo Community Association (OCA)",
+    "summary": "Module 2 summary",
+    "license": "LGPL-3",
+    "installable": True,
+    "auto_install": False,
+    "key": "value3",
+}
