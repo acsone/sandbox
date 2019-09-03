57e6facc80bbbaae603432d88d981e1236f80c3f
sbi <stephane.bidoul@acsone.eu>
2019-09-03 22:57:14 +0200

    odoo_module 12.0.2.4.0

--- odoo_module/__manifest__.py
+++ odoo_module/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "odoo module",
-    "version": "12.0.2.3.8",
+    "version": "12.0.2.4.0",
     "author": "Author, Odoo Community Association (OCA)",
     "summary": "Just extending the summary..................",
     "license": "LGPL-3",
